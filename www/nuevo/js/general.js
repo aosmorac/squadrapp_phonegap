8fb9254b8230bd9ea2be83d8e34b277fa8d0a688
loupdan <mudv@hotmail.es>
2013-10-23 21:20:48 -0700

    ajustes ya avnce lista de busqueda

--- www/nuevo/js/general.js
+++ www/nuevo/js/general.js
@@ -37,6 +37,23 @@ $(function(){
 			$(this).data("estado",'quiet');
 		}
 	})
+	$(document).bind("mobileinit", function(){
+
+		$.mobile.touchOverflowEnabled = false;
+
+		$.mobile.defaultPageTransition = 'none';
+
+		$.mobile.defaultDialogTransition = 'none';
+
+		$.mobile.useFastClick = false
+
+		$.mobile.buttonMarkup.hoverDelay = 0;
+
+		$.mobile.page.prototype.options.domCache = false;
+
+		$.event.special.swipe.scrollSupressionThreshold = 100;
+
+	});
 });
 
 
